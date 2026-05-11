0816e230e44be424405caa409c8cb5af15f4da7d
MD. JAHID HASAN <jahidcjsq@gmail.com>
2026-05-11 19:52:39 +0600

    Add even/odd number check in main function

--- CSE-232/10-5-2026/1.cpp
+++ CSE-232/10-5-2026/1.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+   int n;
+
+   cout << "Enter a number: " << endl;
+   cin >> n;
+
+   if(n%2==0){
+     cout << n << " is a even number" << endl;
+   }
+   else{
+    cout << n << " is a odd number" << endl;
+   }
+}
