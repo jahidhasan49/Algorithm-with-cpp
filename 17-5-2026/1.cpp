3d0a3da08a29e014fbc746f32659b15f90ec5ed3
MD. JAHID HASAN <jahidcjsq@gmail.com>
2026-06-07 19:20:38 +0600

    Update 1.cpp

--- 17-5-2026/1.cpp
+++ 17-5-2026/1.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+using namespace std;
 
+void gradePoint(int n, int arr[]){
+   for(int i=1; i<n; i++){
+     int currentValue=arr[i];
+     int preValue=i-1;
+
+   while(preValue>=0 && arr[preValue] > currentValue){
+      arr[preValue+1]=arr[preValue];
+      preValue--;
+   }
+
+   arr[preValue+1]= currentValue;
+
+   }
+
+}
+
+void showgradepoint(int n, int arr[]){
+   for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+   }
+   cout << endl;
+}
+
+int main(){
+  int n;
+  cout << "Enter number: " << endl;
+  cin >> n;
+
+  int arr[n];
+
+  for(int i=0; i<n; i++){
+    cin >> arr[i];
+  }
+  gradePoint(n, arr);
+  showgradepoint(n, arr);
+}
